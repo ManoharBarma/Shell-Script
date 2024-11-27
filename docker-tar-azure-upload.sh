7655275af5eff00e86c49fdecae6f93c0d9cfa3c
ManoharBarma <manoharbarma07@gmail.com>
2024-11-27 23:34:57 +0530

    Update docker-tar-azure-upload.sh

--- docker-tar-azure-upload.sh
+++ docker-tar-azure-upload.sh
@@ -6,26 +6,19 @@ build_and_tar() {
     CONTAINER_NAME="my-container"         
     STORAGE_ACCOUNT_NAME="mystorageaccount"
     STORAGE_ACCOUNT_KEY="my_storage_key"  
-    TARBALL="${IMAGE_NAME}_${VERSION}.tar"
-    # Docker image build  
-    echo "Building Docker image: $IMAGE_NAME:$VERSION..."  
+    TARFILE="${IMAGE_NAME}_${VERSION}.tar"
+    # script
     docker build -t "$IMAGE_NAME:$VERSION" .  
-
-    # Step 2: Save Docker image as tar file  
-    echo "Saving Docker image to tar file: $TARBALL..."  
-    docker save "$IMAGE_NAME:$VERSION" -o "$TARBALL"  
-
-    # Step 3: Upload tar file to Azure Storage  
-    echo "Uploading tar file to Azure Storage container: $CONTAINER_NAME..."  
+    echo "Docker build success"
+    docker save "$IMAGE_NAME:$VERSION" -o "$TARFILE"  
+    echo "Image saved as $TARFILE"   
     az storage blob upload \
         --account-name "$STORAGE_ACCOUNT_NAME" \
         --account-key "$STORAGE_ACCOUNT_KEY" \
         --container-name "$CONTAINER_NAME" \
-        --file "$TARBALL" \
-        --name "$TARBALL"  
-
-    # Success message  
-    echo "Success: Built Docker image, saved tar file, and uploaded to Azure Storage."  
+        --file "$TARFILE" \
+        --name "$TARFILE"  
+    echo "$TARFILE upload sucess"
 }
-# calling function  
+
 build_and_tar
