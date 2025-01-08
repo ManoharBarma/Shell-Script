09a6a632d906df4b12c8559dc74a9308c332f0f8
ManoharBarma <manoharbarma07@gmail.com>
2025-01-08 05:29:44 -0800

    changed usercreation.sh

--- usercreation.sh
+++ usercreation.sh
@@ -4,15 +4,20 @@
 #description: User creation automation
 #
 #
- if [ $# -gt 0 ]; then
- echo " Creation started "
- for user in $@ ;
- do
-	 existing_user=$(cat /etc/passwd | grep $user | awk -F":" '{ print $1 }')
-	 echo $existing_user
-	 
-
-done
-else 
- echo " pass the valid parameters for user creation "	
- fi
+if [ $# -gt 0 ]; then
+    echo " Creation started "
+    for user in $@; do
+        existing_user=$(grep -w "^$user" /etc/passwd | awk -F":" '{ print $1 }')
+        if [ -n "$existing_user" ]; then
+            echo "user already exists"
+        else
+            echo "creating user $user"
+            sudo useradd $user --shell /bin/bash
+            password="${user}!$(shuf -i 1000-9999 -n 1)"
+            echo "$user:$password" | sudo chpasswd
+            echo "User $user created with password $password"
+        fi
+    done
+else
+    echo " pass the valid parameters for user creation "
+fi
