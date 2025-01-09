87f25efbcab45d911aea707834f19ad0176a6c7a
= <=>
2025-01-09 02:49:03 -0800

    V1

--- usercreation.sh
+++ usercreation.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 #
-#Author: Manohar_Barma
-#description: User creation automation
+# Author: Manohar Barma
+# Description: User creation automation script
+# Version: 1.0
+# Date: 2023-10-25
+# Contact: manoharbarma07@gmail.com
 #
 #
 if [ $# -gt 0 ]; then
-    echo " Creation started "
+    echo "Initiating user creation"
     for user in $@; do
-        existing_user=$(grep -w "^$user" /etc/passwd | awk -F":" '{ print $1 }')
-        if [ -n "$existing_user" ]; then
-            echo "user already exists"
+        if [[ $user =~ ^[a-z]{4}[0-9]{2}$ ]]; then
+            existing_user=$(awk -F":" -v user="$user" '$1 == user { print $1 }' /etc/passwd)
+            if [ -n "$existing_user" ]; then
+                echo "User $user already exists"
+            else
+                echo "Creating user $user"
+                sudo useradd $user --shell /bin/bash
+                password="${user}!$(shuf -i 10000-99999 -n 1)"
+                echo "$user:$password" | sudo chpasswd
+                echo "User \"$user\" created with password \"$password\""
+                sudo passwd -e $user > /dev/null
+                echo "Change the password after first login."
+            fi
         else
-            echo "creating user $user"
-            sudo useradd $user --shell /bin/bash
-            password="${user}!$(shuf -i 1000-9999 -n 1)"
-            echo "$user:$password" | sudo chpasswd
-            echo "User $user created with password $password"
+            echo -e "Invalid username format for $user.\n            It should be 4 characters followed by 2 digits."
+            continue
         fi
     done
 else
-    echo " pass the valid parameters for user creation "
+    echo "Pass the valid parameters for user creation"
 fi
